1a54a3bc7d09aab882a40110eab691098a616d90
unknown <hoatruongquay@gmail.com>
2024-10-01 12:38:36 +0700

    Python và Django #4: Tạo và liên kết các app và url

--- site1/site1/settings.py
+++ site1/site1/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'app1'
 ]
 
 MIDDLEWARE = [
@@ -73,10 +75,20 @@ WSGI_APPLICATION = 'site1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'site1',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+       'HOST': 'localhost',
     }
 }
 
